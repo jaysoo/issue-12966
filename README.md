b94cecf4d9a7ae8317ebca4938743ae684d94dc4
Jack Hsu <jack.hsu@gmail.com>
2023-02-24 10:23:19 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+This is a demo for this issue: https://github.com/nrwl/nx/issues/12966
+
+```
+npm i
+npx nx build api
+```
+
+Inspect `dist/apps/api/main.js` and note that `_GRAPHQL_METADATA_FACTORY` is present.
---
+++ apps/api/src/author.model.ts
@@ -0,0 +1,15 @@
+
+import { ID, Field, ObjectType } from '@nestjs/graphql';
+
+@ObjectType()
+export class Author {
+  @Field(type => ID)
+  id: number;
+
+  @Field({ nullable: true })
+  firstName?: string;
+
+  @Field({ nullable: true })
+  lastName?: string;
+}
+
